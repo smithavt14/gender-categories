bb1ebf6e54fd71168871c3e555b2ede25bf9314b
Alex Smith <smithavt14@gmail.com>
2023-06-05 14:05:49 +0800

    added iterative writing to csv and an offset value if the code ever stops working on a batch.

--- index.js
+++ index.js
@@ -1,37 +1,31 @@
-import { read, write, batch } from './modules/csv.js';
+import { readCategoriesCSV, writeResponseCSV, batch } from './modules/csv.js';
 import { createUserMessage, chatCompletion } from './modules/openai.js';
 
 // 1. Read the categories.csv and add the contents into an array. Output should be an array of strings. 
 const filePath = 'data/categories.csv';
-const responseFilePath = 'data/response.csv';
-let responseArray = [];
+const responseFilePath = 'data/response-iterative.csv';
 const batchSize = 100;
-const categoriesArray = await read(filePath);
+const offset = 9;
+const categoriesArray = await readCategoriesCSV(filePath);
 
 // 2. Batch the array of strings into batches of 10 items. 
 const categoryBatches = batch({ categoriesArray, batchSize });
 
 // 3. For each batch, call GPT 4 API with the system message and add results as a string into the results array. 
-const processResponse = async () => {
-    let i = 0
-    for (let batch of categoryBatches) {
-        console.log(`Fetching results for batch #${i + 1}...`);
-        const userMessage = createUserMessage(batch);
-        const response = await chatCompletion(userMessage);
-        
-        console.log(`Batch #${i + 1} complete!`);
-        i += 1
-        
-        response.content.split('\n').forEach((item) => {
-            let itemArray = item.split(': ');
-            responseArray.push({category: itemArray[0], gender: itemArray[1]});
-        })
-    }
+
+for (offset; offset < categoryBatches.length; offset++) {
+    console.log(`Fetching results for batch #${offset + 1}...`);
+    
+    let responseArray = [];    
+    const userMessage = createUserMessage(categoryBatches[offset]);
+    const response = await chatCompletion(userMessage);
+    
+    console.log(`Batch #${offset + 1} complete!`);
+    
+    response.content.split('\n').forEach((item) => {
+        let itemArray = item.split(': ');
+        responseArray.push({category: itemArray[0], gender: itemArray[1]});
+    })
+
+    await writeResponseCSV({responseArray, responseFilePath});
 }
-
-// 4. write the array of results into the CSV. 
-processResponse().then(() => {
-    responseArray = responseArray.flat();
-    write({responseArray, responseFilePath});
-})
-
--- modules/csv.js
+++ modules/csv.js
@@ -1,7 +1,7 @@
 import fs from 'fs';
 import csv from 'csv-parser';
 
-const read = (filePath) => {
+const readCategoriesCSV = (filePath) => {
     return new Promise((resolve) => {
         console.log('CSV file processing...')
         let categoriesArray = []
@@ -20,6 +20,19 @@ const read = (filePath) => {
     })
 }
 
+const parseResponseCSV = (responseFilePath, responseArray) => {
+    return new Promise((resolve) => {
+        fs.createReadStream(responseFilePath)
+        .pipe(csv())
+        .on('data', (row) => {
+            responseArray.push(row);
+        })
+        .on('end', () => {
+            resolve(responseArray);
+        });
+    })
+}
+
 const batch = ({batchSize, categoriesArray}) => {
     console.log(`Creating batches of ${batchSize} categories...`)
     const batches = [];
@@ -28,30 +41,31 @@ const batch = ({batchSize, categoriesArray}) => {
         const batch = categoriesArray.slice(i, i + batchSize);
         batches.push(batch);
     }
-    console.log(`${batches.length} batches created.`)
+    console.log(`${batches.length} batches created.\n`)
     return batches;
 }
 
-const write = ({ responseArray, responseFilePath }) => {
-    console.log('responseArray: ', responseArray);
-    console.log('Writing to CSV file...');
-    const csvWriter = fs.createWriteStream(responseFilePath);
-
-    // Write the CSV header
-    csvWriter.write('Category,Gender\n');
-
-    // Write the data to the CSV file
-    responseArray.forEach((row) => {
-        const {category, gender} = row;
-        csvWriter.write(`${category},${gender}\n`)
-    });
-
-    // Close the CSV writer
-    csvWriter.end();
-    console.log('Writing complete.');
+const writeResponseCSV = ({ responseArray, responseFilePath }) => {
+    return new Promise( async (resolve) => {
+        console.log('Parsing Existing Response File...')
+        responseArray = await parseResponseCSV(responseFilePath, responseArray);
+        
+        const csvWriter = fs.createWriteStream(responseFilePath);
+    
+        // Write the CSV header
+        csvWriter.write('category,gender\n');
+    
+        // Write the data to the CSV file
+        responseArray.forEach((row) => {
+            const {category, gender} = row;
+            csvWriter.write(`${category},${gender}\n`)
+        });
+    
+        // Close the CSV writer
+        csvWriter.end();
+        console.log(`Completed writing ${responseArray.length} rows.\n`);
+        resolve();
+    })
 }
 
-// Next to do: 
-// Iteratively add to the CSV file so that if it stops half way, can start from where it stopped. 
-
-export { read, write, batch }
+export { readCategoriesCSV, writeResponseCSV, batch }
